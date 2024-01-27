cdcfa2b0f779ed67e11d64f600483b271a1d0f94
Matthew Van Huffel <55860096+Mvanhuffel@users.noreply.github.com>
2024-01-27 15:06:14 +0700

    Update SQL 50.sql

--- SQL/SQL 50.sql
+++ SQL/SQL 50.sql
@@ -11,9 +11,7 @@ where referee_id != 2 or referee_id is null;
 select name, population, area
 from world
 where area >= 3000000 
-
 union
-
 select name, population, area
 from world
 where population >= 25000000;
