ee2ee33c45cf9b7f4e4b756fddcf058102758bd1
Matthew Van Huffel <55860096+Mvanhuffel@users.noreply.github.com>
2024-01-31 18:19:29 +0700

    Update advanced_SQL_50.sql

--- SQL/advanced_SQL_50.sql
+++ SQL/advanced_SQL_50.sql
@@ -34,3 +34,43 @@ WHERE customer_id IN (
       AND SUM(CASE WHEN product_name = 'C' THEN 1 ELSE 0 END) = 0
 )
 ORDER BY customer_id;
+
+-- HIGHEST GRADE FOR EACH STUDENT
+/* Table: Enrollments
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| student_id    | int     |
+| course_id     | int     |
+| grade         | int     |
++---------------+---------+
+(student_id, course_id) is the primary key (combination of columns with unique values) of this table.
+grade is never NULL.
+
+Write a solution to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+Return the result table ordered by student_id in ascending order. */
+
+SELECT 
+  student_id, 
+  course_id, 
+  grade 
+FROM 
+  (
+    SELECT 
+      student_id, 
+      course_id, 
+      grade, 
+      DENSE_RANK() OVER (
+        PARTITION BY student_id 
+        ORDER BY 
+          grade DESC, 
+          course_id ASC
+      ) AS rnk 
+    FROM 
+      Enrollments
+  ) AS ranked 
+WHERE 
+  rnk = 1 
+ORDER BY 
+  student_id;
