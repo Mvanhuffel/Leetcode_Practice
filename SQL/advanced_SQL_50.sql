7959b20d5814060bb3778e6ba56239ef3f6bb159
Matthew Van Huffel <55860096+Mvanhuffel@users.noreply.github.com>
2024-02-01 21:31:02 +0700

    Update advanced_SQL_50.sql

--- SQL/advanced_SQL_50.sql
+++ SQL/advanced_SQL_50.sql
@@ -37,7 +37,6 @@ ORDER BY customer_id;
 
 -- HIGHEST GRADE FOR EACH STUDENT
 /* Table: Enrollments
-
 +---------------+---------+
 | Column Name   | Type    |
 +---------------+---------+
@@ -74,3 +73,38 @@ WHERE
   rnk = 1 
 ORDER BY 
   student_id;
+
+-- COMBINE TWO TABLES
+/* Table: Person
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| personId    | int     |
+| lastName    | varchar |
+| firstName   | varchar |
++-------------+---------+
+personId is the primary key (column with unique values) for this table.
+This table contains information about the ID of some persons and their first and last names.
+
+Table: Address
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| addressId   | int     |
+| personId    | int     |
+| city        | varchar |
+| state       | varchar |
++-------------+---------+
+addressId is the primary key (column with unique values) for this table.
+Each row of this table contains information about the city and state of one person with ID = PersonId.
+
+Write a solution to report the first name, last name, city, and state of each person in the Person table. If the address of a personId is not present in the Address table, report null instead.*/
+
+Select 
+ p.firstname, 
+ p.lastname, 
+ a.city, 
+ a.state 
+from 
+ person p 
+ left join address a on p.personid = a.personid;
