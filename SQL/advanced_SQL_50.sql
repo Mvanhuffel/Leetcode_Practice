8ac0a154dd68eac89b0de11efd37740e27ebb6bc
Matthew Van Huffel <55860096+Mvanhuffel@users.noreply.github.com>
2024-02-02 20:44:21 +0700

    Add 'Sellers With No Sales'

--- SQL/advanced_SQL_50.sql
+++ SQL/advanced_SQL_50.sql
@@ -108,3 +108,52 @@ Select
 from 
  person p 
  left join address a on p.personid = a.personid;
+
+-- SELLERS WITH NO SALES
+
+/*Table: Customer
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| customer_id   | int     |
+| customer_name | varchar |
++---------------+---------+
+customer_id is the column with unique values for this table.
+Each row of this table contains the information of each customer in the WebStore.
+
+Table: Orders
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| sale_date     | date    |
+| order_cost    | int     |
+| customer_id   | int     |
+| seller_id     | int     |
++---------------+---------+
+order_id is the column with unique values for this table.
+Each row of this table contains all orders made in the webstore.
+sale_date is the date when the transaction was made between the customer (customer_id) and the seller (seller_id).
+
+ Table: Seller
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| seller_id     | int     |
+| seller_name   | varchar |
++---------------+---------+
+seller_id is the column with unique values for this table.
+Each row of this table contains the information of each seller.
+
+Write a solution to report the names of all sellers who did not make any sales in 2020.
+Return the result table ordered by seller_name in ascending order.*/
+
+SELECT seller_name 
+FROM seller 
+WHERE seller_id NOT IN (
+    SELECT DISTINCT seller_id 
+    FROM orders 
+    WHERE EXTRACT(YEAR FROM sale_date) = 2020
+)
+ORDER BY seller_name ASC;
+
